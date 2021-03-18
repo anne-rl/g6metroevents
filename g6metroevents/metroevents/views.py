3a3781d8423186f29572575b55c9cdf865de5f99
Mary Anne Labasan <anne-rl@users.noreply.github.com>
2021-03-18 16:38:00 +0800

    additional changes

--- g6metroevents/metroevents/views.py
+++ g6metroevents/metroevents/views.py
@@ -6,23 +6,23 @@ class LandingIndexView(View):
         def get(self, request):
             return render(request, 'index.html')
 
-# class UserRegistrationView(View):
-#         def get(self, request):
-#             return render(request, 'index.html')
+class UserRegistrationView(View):
+        def get(self, request):
+            return render(request, 'index.html')
 
-# class AdministratorView(View):
-#         def get(self, request):
-#             return render(request, 'index.html')
+class AdministratorView(View):
+        def get(self, request):
+            return render(request, 'index.html')
 
-# class OrganizerView(View):
-#         def get(self, request):
-#             return render(request, 'index.html')
+class OrganizerView(View):
+        def get(self, request):
+            return render(request, 'index.html')
 
-# class EventsView(View):
-#         def get(self, request):
-#             return render(request, 'index.html')
+class EventsView(View):
+        def get(self, request):
+            return render(request, 'index.html')
 
-# class ParticipantsView(View):
-#         def get(self, request):
-#             return render(request, 'index.html')
+class ParticipantsView(View):
+        def get(self, request):
+            return render(request, 'index.html')
 
