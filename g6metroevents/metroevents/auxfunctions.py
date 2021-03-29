d21e433a8c39e0e4b9485dc27fd2843e7f17338c
sevenEleven2000 <edmundgmagnojr@gmail.com>
2021-03-29 09:50:54 +0800

    accept admin

--- g6metroevents/metroevents/auxfunctions.py
+++ g6metroevents/metroevents/auxfunctions.py
@@ -6,27 +6,26 @@ from datetime import datetime as dt
 def requestOrganizer(userID):
     user = User.objects.get(id = userID)
     print(user.first_name)
-    request = Request(user = user, type = "requestOrganizer", requestDateTime = dt.now())
+    request = Request.objects.create(user = user, type = "requestOrganizer", requestDateTime = dt.now())
 
     notification = Notification(user = user, notifType = "Request to be Organizer", 
-                                title = "Organizer Role Request", description = "User " + user.first_name + " " + user.last_name + " wants to become an Organizer", 
+                                title = "Send Organizer Role Request", description = "User " + user.first_name + " " + user.last_name + " wants to become an Organizer", 
                                 sentDateTime = dt.now(), request = request)
 
-    request.save()
+    # request.save()
     notification.save()
     print("request Organizer sent")
 
 # request to be admin methods
 def requestAdmin(userID):
     user = User.objects.get(id = userID)
-    print(user.first_name)
-    request = Request(user = user, type = "requestAdmin", requestDateTime = dt.now())
+    request = Request.objects.create(user = user, type = "requestAdmin", requestDateTime = dt.now())
 
     notification = Notification(user = user, notifType = "Request to be Admin", 
-                                title = "Administrator Role Request", description = "User " + user.first_name + " " + user.last_name + " wants to become an Administrator", 
+                                title = "Send Administrator Role Request", description = "User " + user.first_name + " " + user.last_name + " wants to become an Administrator", 
                                 sentDateTime = dt.now(), request = request)
 
-    request.save()
+    # request.save()
     notification.save()
     print("request Admin sent")
 
@@ -34,12 +33,11 @@ def requestJoinEvent(userID, eventID):
     user = User.objects.get(id = userID)
     event = Event.objects.get(id = eventID)
     organizer = event.organizer
-    print("request Join Event sent")
-    request = Request(user = user, event = event, organizer = organizer, type = "joinEvent", requestDateTime = dt.now())
+    request = Request.objects.create(user = user, event = event, organizer = organizer, type = "joinEvent", requestDateTime = dt.now())
     notification = Notification(user = user, event = event, request = request, notifType = "Request to Join Event", organizer = organizer,
                                 title = "Join Event", description = "User " + user.first_name + " " + user.last_name + " wants to join " + event.name + ".", 
                                 sentDateTime = dt.now())
-    request.save()
+    # request.save()
     notification.save()
     print("request Join Event sent")
 
@@ -66,6 +64,7 @@ def declineRequest(requestID):
     request.save()
     user = request.user
     event = request.event
+
     notification = Notification(user = user, event = event, request = request, notifType = "joinEvent", 
                             title = "Request Declined", description = "Your request to join " + event.name + " is declined.", 
                             sentDateTime = dt.now())
@@ -80,3 +79,76 @@ def downvoteEvent(eventID):
     event = Event.objects.get(id = eventID)
     event.upvotes = event.upvotes - 1
     event.save()
+
+def deleteRequest(requestID):
+    request = Request.objects.filter(id = requestID).delete()
+
+def acceptOrganizer(requestID):
+    request = Request.objects.get(id = requestID)
+    request.responseDateTime = dt.now()
+    request.responseStatus = "Accepted"
+    user = request.user
+    request.save()
+    organizer = Organizer()
+    organizer.user = User.objects.get(id=user.id)
+    organizer.save()
+    notification = Notification(user = user, request = request, notifType = "Request to be Organizer", 
+                            title = "Request to be Organizer Accepted", description = "Your request to be Organizer is accepted!", 
+                            sentDateTime = dt.now())
+    # delete all pending user organizer requests as he is already an organizer except newly accepted request
+    user.requests.filter(type = "requestOrganizer", responseStatus="Pending").exclude(id = request.id).delete()
+    user.notifications.filter(notifType = "Request to be Organizer", request_id__responseStatus = "Pending").exclude(request_id = request.id).delete()
+    notification.save()
+
+    
+
+def declineOrganizer(requestID):
+    request = Request.objects.get(id = requestID)
+    request.responseDateTime = dt.now()
+    request.responseStatus = "Declined"
+    user = request.user
+    request.save()
+    request.notifications.all().delete()
+    notification = Notification(user = user, request = request, notifType = "Request to be Organizer", 
+                            title = "Request to be Organizer Declined", description = "Your request to be Organizer is declined!", 
+                            sentDateTime = dt.now())
+    notification.save()
+
+    
+# acceptAdminRequest
+def acceptAdmin(requestID):
+    request = Request.objects.get(id = requestID)
+    request.responseDateTime = dt.now()
+    request.responseStatus = "Accepted"
+    request.save()
+    user = request.user
+    user.is_staff = True
+    user.is_superuser = True
+    user.save()
+    notification = Notification(user = user, request = request, notifType = "Request to be Administrator", 
+                            title = "Request to be Administrator Accepted", description = "Your request to be Administrator is accepted!", 
+                            sentDateTime = dt.now())
+    notification.save()
+
+    # delete all pending user admin role requests as he is already an admin
+    user.requests.filter(type = "requestAdmin", responseStatus="Pending").exclude(id = request.id).delete()
+    user.notifications.filter(notifType = "Request to be Administrator", request_id__responseStatus = "Pending").exclude(request_id = request.id).delete()
+    
+    admin = Administrator()
+    admin.user = User.objects.get(id=user.id)
+    admin.save()
+
+def declineAdmin(requestID):
+    request = Request.objects.get(id = requestID)
+    request.responseDateTime = dt.now()
+    request.responseStatus = "Declined"
+    request.save()
+    request.notifications.all().delete()
+    user = request.user
+    notification = Notification(user = user, request = request, notifType = "Request to be Administrator", 
+                            title = "Request to be Administrator Declined", description = "Your request to be Administrator is declined!", 
+                            sentDateTime = dt.now())
+    notification.save()                        
+    
+    
+    
